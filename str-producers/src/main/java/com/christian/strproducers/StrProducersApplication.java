9c14b9aa1eb4eea7ed47ff44d17090c56a0791f2
ChristianHSousa <chrishsouza@gmail.com>
2025-02-08 17:55:29 -0300

    init

---
+++ str-producers/src/main/java/com/christian/strproducers/StrProducersApplication.java
@@ -0,0 +1,13 @@
+package com.christian.strproducers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProducersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrProducersApplication.class, args);
+    }
+
+}
---
+++ str-producers/src/test/java/com/christian/strproducers/StrProducersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christian.strproducers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
